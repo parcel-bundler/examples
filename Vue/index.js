57aae35eb362696edf9c074297c49947f82419ff
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2017-12-16 11:46:39 +0530

    Updated the example
    - [x] Removed the CDN requirement.
    - [x] added `css` import example.
    - [x] Updated the UI to look similar as other examples.

--- Vue/index.js
+++ Vue/index.js
@@ -1,18 +1,13 @@
+import Vue from 'vue/dist/vue'
+import './index.css';
+
 new Vue({
   el: '#app',
-  data: {
-    greeting: 'Welcome to your Vue.js app!',
-    docsURL: 'http://vuejs.org/guide/',
-    discordURL: 'https://chat.vuejs.org',
-    forumURL: 'http://forum.vuejs.org/'
-  },
-  methods: {
-    humanizeURL: function (url) {
-      return url
-        .replace(/^https?:\/\//, '')
-        .replace(/\/$/, '')
-    }
-  }
+  template: `
+    <div id="app">
+      <h1>Hello World 📦 🚀</h1>
+    </div>
+  `
 });
 
 if (module.hot) {
