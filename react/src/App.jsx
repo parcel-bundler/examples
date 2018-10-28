1f588ad7fb7d4be47afd72d0b1454b4deca8bd88
jthegedus <jthegedus@jttech.io>
2018-10-28 19:49:12 +1100

    add readme, run prettier, use abs paths, update index.html to be in src/

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import './App.css'
+import '/App.css'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World 📦 🚀</h1>
+        <h1>Hello World from React! 📦 🚀</h1>
       </div>
     )
   }
