f607bec719c9c9e3cddb853a3597b022b890f91e
jthegedus <jthegedus@jttech.io>
2018-10-28 15:27:20 +1100

    update react example deps

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,10 +1,12 @@
-import React,  { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 export default class App extends Component {
   render() {
-    return <div>
-      <h1>Hello World 📦 🚀</h1>
-    </div>;
+    return (
+      <div>
+        <h1>Hello World 📦 🚀</h1>
+      </div>
+    )
   }
 }
