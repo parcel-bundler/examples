43a11c59a1b3d5686442dc0afba1be405dbd4a89
jthegedus <jthegedus@jttech.io>
2018-10-30 12:23:28 +1100

    separate Flow examples. Simple & abs mod res example

--- flow-with-absolute-module-resolution/src/main.js
+++ flow-with-absolute-module-resolution/src/main.js
@@ -7,4 +7,4 @@ let ripeApple: boolean = apple.ripe;
 let bananaColor: boolean = banana.color;
 
 console.log("is the apple ripe?", ripeApple);
-console.log("what is the banana's color", bananaColor);
+console.log("what is the banana's color?", bananaColor);
